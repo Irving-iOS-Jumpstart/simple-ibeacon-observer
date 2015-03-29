2e98747a92d13b9f40b36d00fca5830ce5df327c
James Derry <jderry@derryitconsulting.com>
2015-03-29 15:51:20 -0500

    Initial Commit

---
+++ ibeacon-observer-orig/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ibeacon-observer-orig
+//
+//  Created by James Derry on 3/29/15.
+//  Copyright (c) 2015 James Derry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
